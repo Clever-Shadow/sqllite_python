72222c0f6858d187e84fa80108fb5dc69cefde09
Евгений Лосев <losevivc@iatu.local>
2021-05-14 16:10:28 +0400

    #1 Create app

--- insert_to_db.py
+++ insert_to_db.py
@@ -4,11 +4,11 @@ import sqlite3
 # Обращаемся к БД, если её нет, то она автоматически создается
 connection = sqlite3.connect("test.db")
 
-# Чтобы создать таблицу в базе данных, нам нужно использовать объект курсора
+# Чтобы выполнить запрос к таблице в базе данных, нам нужно использовать объект курсора
 cursor = connection.cursor()
 
 # Теперь можем выполнить запрос к БД
-cursor.execute("CREATE TABLE table_name (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `column2` text, `column3` text);")
+cursor.execute("INSERT INTO table_name (`column2`, `column3`) VALUES ('value1', 'value2');")
 
 # Фиксируем изменения в БД
 connection.commit()
